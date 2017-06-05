a873979d0c9795fc0b2e6d0d9f118665dc259be3
mengskysama <mengskysama@gmail.com>
2017-06-05 22:12:43 +0800

    add docker

--- docker/install.sh
+++ docker/install.sh
@@ -1,3 +1,10 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+OPS_DIR=/opt/ops
+
 # docker-ce
 apt-get update
 apt-get -y install software-properties-common
@@ -10,7 +17,9 @@ add-apt-repository \
 apt-get update
 apt-get -y install docker-ce
 
+# logrotate
+cp $OPS_DIR/docker/files/logrotate /etc/logrotate.d/docker-container
+
 # docker-compose
 apt-get install python3-pip
 pip3 install docker-compose
-
---
+++ rar/install.sh
@@ -0,0 +1,5 @@
+wget http://rarlab.com/rar/rarlinux-x64-5.5.b3.tar.gz
+tar -xzvf rarlinux-x64-5.5.b3.tar.gz
+cd rar
+make
+sudo make install
