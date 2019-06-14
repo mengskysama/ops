160cb07015cd0842491247e5f3deda382cc30717
mengskysama <mengskysama@gmail.com>
2019-06-15 02:03:18 +0800

    Update install.sh

--- openresty/install.sh
+++ openresty/install.sh
@@ -31,13 +31,32 @@ tar xzvf $VER.tar.gz
 cd $VER
 
 ./configure \
---with-http_realip_module \
---with-http_iconv_module \
 --with-pcre-jit \
---with-http_ssl_module \
+--with-stream_ssl_module\
+--with-stream_ssl_preread_module \
+--with-http_v2_module \
+--without-mail_pop3_module \
+--without-mail_imap_module \
+--without-mail_smtp_module \
 --with-http_stub_status_module \
+--with-http_realip_module \
+--with-http_addition_module \
+--with-http_auth_request_module  \
+--with-http_secure_link_module \
+--with-http_random_index_module \
+--with-http_gzip_static_module \
+--with-http_sub_module \
+--with-http_dav_module \
+--with-http_flv_module \
+--with-http_mp4_module \
+--with-http_gunzip_module \
+--with-threads \
+--with-dtrace-probes \
 --with-stream \
---with-stream_ssl_module \
+--with-stream_ssl_preread_module \
+--with-http_ssl_module \
+--with-http_iconv_module \
+--with-http_slice_module \
 --add-module=/tmp/stream-lua-nginx-module-$STREAM_VER
 
 make
